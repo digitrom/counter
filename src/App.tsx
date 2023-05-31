85a7d04a7eb2acf3410d7c70531562a16b5912d7
digitrom <roma.bobrovski@gmail.com>
2023-05-31 23:47:59 +0200

    counter in progress 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,56 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Button from "./Button";
 
 function App() {
-
     const [value, setValue] = useState<number>(0)
-    console.log(value)
-    const increment = () => {
+
+    useEffect(()=> {
+        getFromLocalStorageHandler()
+    }, [])
+
+    useEffect(()=> {
+        setToLocalStorageHandler()
+    }, [value])
+
+    const incHandler = () => {
         setValue(value + 1)
+        // setToLocalStorageHandler()
     }
+
+    const resHandler = () => {
+        setValue(0)
+        // clearLocalStorageHandler()
+
+    }
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value) )
+    }
+
+    const getFromLocalStorageHandler = () => {
+        // debugger
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }
+
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+    }
+
+    console.log(value)
     return (
         <div className="App">
-            <Button name={'inc'} callback={increment}/>
+            <div className="btn">
+                <Button name={'inc'} callback={incHandler}/>
+                <Button name={'res'} callback={resHandler}/>
+                <Button name={'set'} callback={setToLocalStorageHandler}/>
+                {/*<Button name={'get'} callback={getFromLocalStorageHandler}/>*/}
+            </div>
+            {value}
         </div>
     );
 }
